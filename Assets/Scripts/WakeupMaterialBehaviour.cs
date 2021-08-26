224823971fdc5a01085c6b388c84a5a3afb1aa35
Lucas <s208055>
2021-08-26 12:15:55 -0500

    created a box that changes its materials
    box has a WakeupMaterialBehaviour

---
+++ Assets/Scripts/WakeupMaterialBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class WakeupMaterialBehaviour : MonoBehaviour
+{
+    public Material AwakeMaterial = null;
+    public Material asleepMaterial = null;
+
+    private Rigidbody _rigidBody = null;
+    private MeshRenderer _renderer = null;
+
+    private bool _wasAsleep = false;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _renderer = GetComponent<MeshRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        //set material to asleep if rigidBody is asleep
+        if (_wasAsleep && _rigidBody.IsSleeping() && asleepMaterial)
+        {
+            _wasAsleep = false;
+            _renderer.material = asleepMaterial;
+        }
+        //set material to awake if rigidBody is awake
+        else if (!_wasAsleep && !_rigidBody.IsSleeping() && AwakeMaterial)
+        {
+            _wasAsleep = true;
+            _renderer.material = AwakeMaterial;
+        }
+    }
+
+}
