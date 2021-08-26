e9577a052bb7746139413c0c4a38ae6790ffe7f7
Lucas <s208055>
2021-08-26 14:48:14 -0500

    added a brick house and cannonball

---
+++ Assets/Scripts/CannonBallBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallBehaviour : MonoBehaviour
+{
+    public float forceOnFire = 300;
+
+    private bool fire = false;
+    private bool canFire = true;
+
+    private Rigidbody _rigidBody = null;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _rigidBody.isKinematic = true;
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && canFire)
+        {
+            _rigidBody.isKinematic = false;
+            _rigidBody.AddForce(transform.forward * forceOnFire,ForceMode.VelocityChange);
+            canFire = false;
+        }
+    }
+}
