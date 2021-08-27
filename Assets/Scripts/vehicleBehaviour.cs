8d7b88c8d85cbc9b51004b594f4d2bb30b2235c0
Lucas <s208055>
2021-08-27 15:59:35 -0500

    adding car movement using hinge joints

---
+++ Assets/Scripts/vehicleBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vehicleBehaviour : MonoBehaviour
+{
+    public HingeJoint frontLeft;
+    public HingeJoint frontRight;
+    public HingeJoint backLeft;
+    public HingeJoint backRight;
+
+    private JointMotor m_frontLeftMotor;
+    private JointMotor m_frontRightMotor;
+    private JointMotor m_backLeftMotor;
+    private JointMotor m_backRightMotor;
+
+    private void Awake()
+    {
+        m_frontLeftMotor = frontLeft.motor;
+        m_frontRightMotor = frontRight.motor;
+        m_backLeftMotor = backLeft.motor;
+        m_backRightMotor = backRight.motor;
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
