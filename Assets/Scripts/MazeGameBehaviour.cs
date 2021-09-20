672ce2d8b8b8f2af31b510fbd4e8959cafeec204
lucas <umbreonbeyond@gmail.com>
2021-09-20 12:17:49 -0500

    modified the scene to better suit the player

---
+++ Assets/Scripts/MazeGameBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGameBehaviour : MonoBehaviour
+{
+    public GameObject showObject;
+    public collectBehaviour collection;
+    private void Update()
+    {
+        if (collection.RewardsCollected >= collection.winAmount - 1)
+        {
+            showObject.SetActive(true);
+        }
+        else
+            showObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Player/collectBehaviour.cs
+++ Assets/Scripts/Player/collectBehaviour.cs
@@ -7,24 +7,25 @@ public class collectBehaviour : MonoBehaviour
     public Animator _imageAnim;
     public string SpecialSceneName;
     public int winAmount = 1;
-    private int rewardsCollected = 0;
+    private int _rewardsCollected = 0;
+    public int RewardsCollected { get { return _rewardsCollected; } }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Collectable"))
         {
-            rewardsCollected++;
+            _rewardsCollected++;
             Destroy(collision.gameObject);
         }
         if (collision.gameObject.CompareTag("Special"))
         {
             _imageAnim.gameObject.GetComponent<FadeToBlack>().sceneIndex = SpecialSceneName;
-            rewardsCollected++;
+            _rewardsCollected++;
             Destroy(collision.gameObject);
         }
     }
     private void Update()
     {
-        if(rewardsCollected >= winAmount)
+        if(_rewardsCollected >= winAmount)
             _imageAnim.SetBool("FadeToBlack", true);
     }
 }
